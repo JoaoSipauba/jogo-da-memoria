8c9778a90ff31ad2e9ec6f8e1f7e3e6720f9e027
JoaoSipauba <joaosipauba@hotmail.com>
2019-11-15 13:34:12 -0300

    formatação dos codigos.

--- src/js/inicial.js
+++ src/js/inicial.js
@@ -1,24 +1,26 @@
 if (localStorage.getItem('top1') === null || localStorage.getItem('top1') === '800') {
     document.write('')
 }
+
 else {
     var rankTxt = `<h2>Recorde de ${localStorage.getItem('recordista')} em ${localStorage.getItem('top1')}s</h2>`
     document.write(rankTxt)
 }
 $('#btn_iniciar').on('click', function () {
     var player = document.getElementById('inputName').value;
-    if (player === ''){
-        $('#inputName').attr('required','req');
+    if (player === '') {
+        $('#inputName').attr('required', 'req');
     }
-    if (player !== ''){
+
+    if (player !== '') {
         localStorage.setItem('player', player);
-        window.location.href="main.html";
+        window.location.href = "main.html";
     }
 })
 
-$('#inputName').keyup(function(event){ 
-    var inputValue = event.target.value.trim()  
-    if (inputValue !== ''){                 
+$('#inputName').keyup(function (event) {
+    var inputValue = event.target.value.trim()
+    if (inputValue !== '') {
         $('#inputName').removeAttr('required');
-    }                          
+    }
 })
